067ff67063127af1741a5cc1e3a297e06cf38ba4
Rhea Parekh <rheaparekh@Rheas-MacBook-Air.local>
2017-03-22 03:24:43 +0530

    initial migrations done

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from polls import views
 
 urlpatterns= patterns('',
   url(r'^$',views.index, name='index'),
+  url(r'^(?P<question_id>\d+)/$',view.details,name='detail'),
+  url(r'^(?P<question_id)\d+)/results/$',view.results,name='results'),
+  url(r'^(?P<question_id>\d+)/vote/$',views.vote,name='vote'),
 )
-
-
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-     return HttpResponse("sdsd")
+def details(request,question_id):
+     return HttpResponse("Question: %s" %question_id)
+
+def results(request,question_id):
+      response="Results of Question: %s"
+      return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+      return HttpResponse("You're voting on question %s." % question_id)
