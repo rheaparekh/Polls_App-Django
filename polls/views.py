2522341416e849fc0c770c78bf13d99fbfd1ed54
Rhea Parekh <rheaparekh12@gmail.com>
2017-03-23 02:21:19 +0530

    Details page added

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,Http404
 from polls.models import Question,Choice
 from django.template import RequestContext, loader
@@ -9,7 +9,8 @@ def index(request):
      return render (request,'polls/index.html',question_list)
 
 def details(request,question_id):
-     return HttpResponse("Question: %s" %question_id)
+     question=get_object_or_404(Question,pk=question_id)
+     return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
       response="Results of Question: %s"
--- pollsApp/urls.py
+++ pollsApp/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls',namespace="polls")),
 )
