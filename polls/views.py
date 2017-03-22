515e4f0397004c79454bfeb7557526a42aa7181f
Rhea Parekh <rheaparekh12@gmail.com>
2017-03-23 00:46:29 +0530

    Question displayed, templates added

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+from polls.models import Question,Choice
+from django.template import RequestContext, loader
+
+def index(request):
+     latest_question_list= Question.objects.order_by('-pub_date')[:5]
+     question_list={'latest_question_list':latest_question_list}
+     return render (request,'polls/index.html',question_list)
 
 def details(request,question_id):
      return HttpResponse("Question: %s" %question_id)
